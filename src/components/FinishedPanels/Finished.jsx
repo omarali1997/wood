033b737544a384dad35d979166f020e18676da85
Iyad Saadeh <93885250+IyadSaadeh@users.noreply.github.com>
2022-12-17 17:58:19 +0300

    lala

--- src/components/FinishedPanels/Finished.jsx
+++ src/components/FinishedPanels/Finished.jsx
@@ -1,73 +1,96 @@
-import React from "react";
-import './Finished.css'
+import React, { useState } from "react";
+import { Card } from "react-bootstrap";
+import "./Finished.css";
 
+const Image=()=> {
+  const [showDetails, setShowDetails] = useState(false);
+  const images = [
+    {
+      title: "Melamine",
+      src: "https://wood.digit-tips.com/wp-content/uploads/2022/09/1-img.png",
+      alt: "image 1",
+      details: "https://wood.digit-tips.com/wp-content/uploads/2022/09/2.png",
+    },
+    {
+      title: "Paper over laid",
 
-function Image({ src, alt, details }) {
-    const [showDetails, setShowDetails] = React.useState(false);
-  
-    return (
-      <div
+      src: "https://wood.digit-tips.com/wp-content/uploads/2022/09/2-img.png",
+      alt: "image 2",
+      details: "https://wood.digit-tips.com/wp-content/uploads/2022/09/2.png",
+    },
+    {
+      title: "Veneered",
+
+      src: "https://wood.digit-tips.com/wp-content/uploads/2022/09/3-img.png",
+      alt: "image 3",
+      details: "https://wood.digit-tips.com/wp-content/uploads/2022/09/3.png",
+    },
+    {
+      title: "High glass + super matt",
+
+      src: "https://wood.digit-tips.com/wp-content/uploads/2022/09/4-img.png",
+      alt: "image 4",
+      details: "https://wood.digit-tips.com/wp-content/uploads/2022/09/5.png",
+    },
+    {
+      title: "Moulded door skin",
+
+      src: "https://wood.digit-tips.com/wp-content/uploads/2022/09/5-img.png",
+      alt: "image 5",
+      details: "https://wood.digit-tips.com/wp-content/uploads/2022/09/5.png",
+    },
+    {
+      title: "Polyester faced panel",
+
+      src: "https://wood.digit-tips.com/wp-content/uploads/2022/09/6-img.png",
+      alt: "image 6",
+      details: "https://wood.digit-tips.com/wp-content/uploads/2022/09/6.png",
+    },
+    {
+      title: "Film faced plywood",
+
+      src: "https://wood.digit-tips.com/wp-content/uploads/2022/09/7.png",
+      alt: "Film faced plywood",
+      details: null,
+    },
+    // Add more images here
+  ];
+  return (
+    <div className="hoverImg">
+    {images.map((image) => (
+      <>
+      <Card className="card_finishedPanel">
+        <Card.Header style={{border: 'none'}}>
+          <Card.Text>
+          {image.title}
+            </Card.Text> 
+        </Card.Header>
+        <Card.Body style={{border: 'none'}}>
+        <Card.Img src={image.src} alt={image.alt}
         onMouseEnter={() => setShowDetails(true)}
         onMouseLeave={() => setShowDetails(false)}
-      >
-        <img src={src} alt={alt} />
-        {showDetails && <div><img src={details} alt={alt} /></div>}
-      </div>
-    );
-  }
+        ></Card.Img>
+       
+        </Card.Body>
+        {showDetails?(
+          <Card.Footer style={{border: 'none'}}>
+          {image.details != null ? <Card.Img src={image.details} alt={image.alt}></Card.Img>: ""}
+        
+
+        </Card.Footer>
+        ):""}
+      </Card>
+      {/* <Image
+          key={image.src}
+          src={image.src}
+          alt={image.alt}
+          details={image.details != null ? image.details : ""}
+        /> */}
+      </>
+    ))}
+  </div>
   
-  // Parent component
-  function ImageGallery() {
-    const images = [
-      {
-        src: 'https://wood.digit-tips.com/wp-content/uploads/2022/09/1-img.png',
-        alt: 'image 1',
-        details: 'https://wood.digit-tips.com/wp-content/uploads/2022/09/2.png'
-      },
-      {
-        src: 'https://wood.digit-tips.com/wp-content/uploads/2022/09/2-img.png',
-        alt: 'image 2',
-        details: 'https://wood.digit-tips.com/wp-content/uploads/2022/09/2.png'
-      },
-      {
-        src: 'https://wood.digit-tips.com/wp-content/uploads/2022/09/3-img.png',
-        alt: 'image 3',
-        details: 'https://wood.digit-tips.com/wp-content/uploads/2022/09/3.png'
-      },
-      {
-        src: 'https://wood.digit-tips.com/wp-content/uploads/2022/09/4-img.png',
-        alt: 'image 4',
-        details: 'https://wood.digit-tips.com/wp-content/uploads/2022/09/5.png'
-      },
-      {
-        src: 'https://wood.digit-tips.com/wp-content/uploads/2022/09/5-img.png',
-        alt: 'image 5',
-        details: 'https://wood.digit-tips.com/wp-content/uploads/2022/09/5.png'
-      },
-      {
-        src: 'https://wood.digit-tips.com/wp-content/uploads/2022/09/6-img.png',
-        alt: 'image 6',
-        details: 'https://wood.digit-tips.com/wp-content/uploads/2022/09/6.png'
-      },
-      {
-        src: 'https://wood.digit-tips.com/wp-content/uploads/2022/09/7.png',
-        alt: 'Film faced plywood',
-        // details: 'Image 2 details'
-      },
-      // Add more images here
-    ];
-  
-    return (
-      <div className="hoverImg" >
-        {images.map(image => (
-          <Image
-            key={image.src}
-            src={image.src}
-            alt={image.alt}
-            details={image.details}
-          />
-        ))}
-      </div>
-    );
-  }
-  export default ImageGallery;
+  );
+}
+
+export default Image;
